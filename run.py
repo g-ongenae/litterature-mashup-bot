f1ce26462540cb15d142dfab34c3cd915ca7dc61
Guillaume Ongenae <guillaume.ongenae@gmail.com>
2020-06-07 18:00:03 +0200

    add server and fix Procfile

--- run.py
+++ run.py
@@ -1,3 +1,18 @@
+from multiprocessing import Process
 from src import main
+from src.server import run_server
 
-main()
+
+def run_in_parallel(tasks):
+    """
+    Run tasks in Parallel
+    """
+    running_tasks = [Process(target=task) for task in tasks]
+    for running_task in running_tasks:
+        running_task.start()
+    for running_task in running_tasks:
+        running_task.join()
+
+
+if __name__ == "__main__":
+    run_in_parallel([main, run_server])
---
+++ src/server/__init__.py
@@ -0,0 +1,43 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+from os import getenv
+import time
+
+
+class Handler(BaseHTTPRequestHandler):
+    """
+    Simple request handler to respond on GET request
+    """
+
+    def do_GET(self):  # pylint: disable=invalid-name
+        """
+        Respond on a GET request
+        """
+        self.send_response(200)
+        self.send_header("Content-type", "text/plain")
+        self.end_headers()
+
+        self.server.output["time"] = time.asctime()
+        message = """
+        Server to run https://twitter.com/LitMashupBot.
+        A Twitter bot that mashups two French writers and creates a cover.
+        For more information, please contact <guillaume.ongenae@gmail.com> or https://twitter.com/GOngenae.
+
+        Created by Guillaume Ongenae (https://g-ongenae.github.com/)
+        """
+        self.server.output["body"] = message
+
+        output = ""
+        for key in self.server.output:
+            output = output + key + ": " + self.server.output[key] + "\n"
+
+        self.wfile.write(output.encode())
+
+
+def run_server():
+    """
+    Run the server
+    """
+    httpd = HTTPServer(("localhost", getenv("PORT")), Handler)
+    httpd.output = {}
+    httpd.output["name"] = "Literature Mashup Bot — HTTP Server"
+    httpd.serve_forever()
