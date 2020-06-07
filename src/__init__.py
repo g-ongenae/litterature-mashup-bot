c4a3473034d84b7929b96e3db75bcedcbfc12ca8
Guillaume Ongenae <guillaume.ongenae@gmail.com>
2020-06-07 11:58:06 +0200

    connect and schedule to tweet everyhour

--- src/__init__.py
+++ src/__init__.py
@@ -1,23 +1,76 @@
-from os import getenv
+from os import getenv, remove
+import tweepy
+import schedule
 from .images.gallimart import make_image
 from .names.author import Author, mashup_names
 from .names.books_type import BooksType
 from .names.books import Books
 
-API_KEY = getenv("API_KEY")
+# Global variables
+AUTHOR = None
+BOOK_TYPES = None
+BOOKS = None
+TWITTER = None
+
+
+def init_tweeter(
+    consumer_key: str, consumer_secret: str, access_token: str, access_token_secret: str
+):
+    """
+    Log on Twitter API with tweepy
+    """
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    return tweepy.API(auth)
+
+
+def tweet() -> None:
+    """
+    Tweet the mashup
+    """
+    filepath = "./image_test.jpg"
+    # Get the author to mashup
+    author_names = AUTHOR.get_random_names()
+    # Mashup info
+    new_author_name = mashup_names(author_names)
+    new_book_title = BOOKS.mashup_names(author_names)
+    new_book_type = BOOK_TYPES.get_random_books_type()
+    # Create the mashup image and save it
+    image = make_image(new_author_name, new_book_title, new_book_type)
+    image.save(filepath)
+    # Tweet the image with the info
+    message = "{authors[0]} + {authors[1]} = {new_author}".format(
+        authors=author_names, new_author=new_author_name
+    )
+    TWITTER.update_with_media(filepath, status=message)
+    remove(filepath)
 
 
 def main() -> None:
     """
     Main
     """
-    author = Author()
-    books_type = BooksType()
-    books = Books(api_key=API_KEY)
+    # Declare the global variables
+    global AUTHOR
+    global BOOK_TYPES
+    global BOOKS
+    global TWITTER
 
-    author_names = author.get_random_names()
-    new_author_name = mashup_names(author_names)
-    new_book_title = books.mashup_names(author_names)
-    new_book_type = books_type.get_random_books_type()
-    image = make_image(new_author_name, new_book_title, new_book_type)
-    image.save("./image_test.jpg")
+    # Init the global variables
+    AUTHOR = Author()
+    BOOK_TYPES = BooksType()
+    BOOKS = Books(api_key=getenv("GBOOKS_API_KEY"))
+    TWITTER = init_twiter(
+        consumer_key=getenv("TWITTER_CONSUMER_KEY"),
+        consumer_secret=getenv("TWITTER_ONSUMER_SECRET"),
+        access_token=getenv("TWITTER_ACCESS_TOKEN"),
+        access_token_secret=getenv("TWITTER_ACCESS_TOKEN_SECRET"),
+    )
+
+    # Create cron to tweet once every hour
+    schedule.every().hour.do(tweet)
+
+    # Run
+    while 1:
+        schedule.run_pending()
+        time.sleep(1)
